715f75feedae9ed73727f32063cbc22b0592ebdf
turab202 <turabturqb@gmail.com>
2025-12-21 15:48:21 +0300

    changes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -27,13 +27,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // hash password before saving user to db
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 // compare password func
